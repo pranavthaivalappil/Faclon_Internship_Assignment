aebb7e36ea6ab45f1bd4610c2f07d501114c935c
Pranav Thaivalappil <thaivalappilpranav@gmail.com>
2026-02-09 21:14:12 +0530

    Connected MongoDb Atlas using Mongoose

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
+
+
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
+// middleware
 app.use(express.json());
 
+// test route
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send("Server is running ");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  })
+})
+
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  });
+
+
+
